e96e53c9c6c3afc0eb78a96ca56927e66bd5b516
Yugendra N <yugenjr847@gmail.com>
2026-01-12 10:44:26 +0530

    safeguarded credentials using .env file

--- frontend/services/firebase.ts
+++ frontend/services/firebase.ts
@@ -1,17 +1,23 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth } from 'firebase/auth';
+import { getAuth } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-// Firebase config (Web app config from Firebase Console)
+import { 
+  FIREBASE_API_KEY, 
+  FIREBASE_AUTH_DOMAIN, 
+  FIREBASE_PROJECT_ID, 
+  FIREBASE_STORAGE_BUCKET, 
+  FIREBASE_MESSAGING_SENDER_ID, 
+  FIREBASE_APP_ID 
+} from '@env';
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCZ7uSTOO0uj3yKAxB5jIvXr-5pl25k1qo",
-  authDomain: "onevoice-auth.firebaseapp.com",
-  projectId: "onevoice-auth",
-  storageBucket: "onevoice-auth.firebasestorage.app",
-  messagingSenderId: "199969728038",
-  appId: "1:199969728038:web:ff97e2b64d1b0667cb68e7",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
